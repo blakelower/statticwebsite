9feab96825b32574ed7c35b602d9a24b809a367a
blakelower <blakelower1999@gmail.com>
2019-12-19 17:07:04 +0700

    wip

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-console.log("this is working");
+console.log("this is working and it is great");
 const greet = document.querySelector(".hello");
 greet.textContent = "this is working fors ure AND IT IS AWESOME";
